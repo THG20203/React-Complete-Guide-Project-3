7d4e19fa14a4908de202e9d895b638ebed4f54c7
THG20203 <tristanhgriffiths8@yahoo.com>
2023-04-30 13:15:43 +0100

    inital user markup

--- src/components/UI/Users/AddUser.js
+++ src/components/UI/Users/AddUser.js
@@ -1,8 +1,22 @@
 import React from "react";
 
-const AddUser = (props) => {};
+const AddUser = (props) => {
+  return (
+    <form>
+      <label htmlFor="username">Username</label>
+      {/* htmlFor = prop name for assigning that attribute to a label, 
+      connect with input with username value. */}
+      <input id="username" type="text" />
+      {/* input -> user to type in username, hence type text */}
+      <label htmlFor="username">Age (Years)</label>
+      <input id="username" type="number"></input>
+    </form>
+  );
+};
 /* AddUser function arrow function stored within a const, could have 
 done function AddUser() {} but chose not to */
+/* inside of 'AddUser' -> render something on the screen, allows user to 
+enter a name and an age, and have button to confirm all of that. */
 
 export default AddUser;
 /* export as default, so can import that component into another file
