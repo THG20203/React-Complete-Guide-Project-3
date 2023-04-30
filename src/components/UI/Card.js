4a05da3dd6857fd5c6b15f12ecc4855ff81f3db4
THG20203 <tristanhgriffiths8@yahoo.com>
2023-04-30 14:11:14 +0100

    props.children explanation

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,10 +2,15 @@ import React from "react";
 
 const Card = (props) => {
   return (
-    <div>
+    /* assinging a class to the div so we can style it, use CSS modules */
+    <div className="">
       {/* Card component should output whats passed between the opnening and 
-    closing tags of the card. Access this content with special children prop, 
-    put it between curly braces. */}
+    closing tags of the card. */}
+      {/* Access this content with special children prop, put it between curly braces.
+    We are putting it between curly braces because its a JavaScript expression rather
+    than JSX code. This JavaScript expression should be evaluated inside of JSX code. */}
+      {/* props.children will give us that content which has passed between opening and 
+    closing tag of the card component */}
       {props.children}
     </div>
   );
