f5d5c8198d24a455a515697ec933f838e13ab551
THG20203 <tristanhgriffiths8@yahoo.com>
2023-04-30 13:09:15 +0100

    arrow function react component refresher

---
+++ src/components/UI/Users/AddUser.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AddUser = (props) => {};
+/* AddUser function arrow function stored within a const, could have 
+done function AddUser() {} but chose not to */
+
+export default AddUser;
+/* export as default, so can import that component into another file
+and use it there */
