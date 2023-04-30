8a552b0565099b979067db171f040a35f5e4b292
THG20203 <tristanhgriffiths8@yahoo.com>
2023-04-30 18:00:02 +0100

    implementation of the button on adduser

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "../UI/Card";
 /* remember for non JavaScript files in react need to add file extension, i.e. .css */
 import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -25,7 +26,7 @@ const AddUser = (props) => {
         <input id="username" type="text" />
         <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
