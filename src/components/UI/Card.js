c4703dc291c9725d13062c5773c354c09e44b996
THG20203 <tristanhgriffiths8@yahoo.com>
2023-04-30 14:16:09 +0100

    css modules card component

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,9 +1,13 @@
 import React from "react";
+/* CSS modules, need to alter import a little bit -> has to be class from */
+import classes from "./Card.module.css";
 
 const Card = (props) => {
   return (
-    /* assinging a class to the div so we can style it, use CSS modules */
-    <div className="">
+    /* assinging a class to the div so we can style it, use CSS modules. Access the 
+    card class with classes.card. This will assign this generated unique class name to 
+    this div */
+    <div className={classes.card}>
       {/* Card component should output whats passed between the opnening and 
     closing tags of the card. */}
       {/* Access this content with special children prop, put it between curly braces.
