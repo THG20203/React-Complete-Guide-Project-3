4ea4f808d94509de80b3e89c1bfe14307b8d1bfc
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-01 12:21:08 +0100

    more markup of button

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Card from "./Card";
+/* because have button to close the modal, importing the button -> re use the button 
+component -> Re usable building blocks. */
+import Button from "./Button";
 
 /* ErrorModal gets props */
 const ErrorModal = (props) => {
@@ -7,6 +10,19 @@ const ErrorModal = (props) => {
   return (
     <Card>
       {/* Inside of Card component, modal should have different sections. */}
+      <header>
+        {/* h2 tag -> output a title */}
+        <h2></h2>
+      </header>
+      <div>
+        {/* <div> element to hold main content -> paragraph with the error message want to 
+        display eventually. */}
+        <p></p>
+      </div>
+      <footer>
+        {/* In footer want to have button we can click to close the modal */}
+        <Button>Okay</Button>
+      </footer>
     </Card>
   );
 };
