5f677359d952681274d8333cb39a3a6dffede395
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-01 11:57:49 +0100

    very basic layout and styles of ErrorModal

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Card from "./Card";
+
+/* ErrorModal gets props */
+const ErrorModal = (props) => {
+  return;
+  /* Using Card as a wrapper -> like the rounded corners */
+  <Card></Card>;
+};
+
+export default ErrorModal;
