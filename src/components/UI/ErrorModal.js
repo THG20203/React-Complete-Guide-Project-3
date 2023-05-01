9680d971721ecf8c25ea99a6266b7fb8702520b5
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-01 12:39:09 +0100

    dynamic markup for modal with props created

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -11,13 +11,20 @@ const ErrorModal = (props) => {
     <Card>
       {/* Inside of Card component, modal should have different sections. */}
       <header>
-        {/* h2 tag -> output a title */}
-        <h2></h2>
+        {/* h2 tag -> output a title. Modal should be configurable from the outside.
+        So, don't want to hard code the title and the message into this component. it 
+        should also be reusable. (Can use it in different places of the app, to show
+        different error messages). */}
+        {/* Hence, outputting something dynamic between curly braces -> use props.title 
+        for example. Can be props.anythingIwant -> its my component */}
+        <h2>{props.title}</h2>
       </header>
       <div>
         {/* <div> element to hold main content -> paragraph with the error message want to 
         display eventually. */}
-        <p></p>
+        {/* similar to title above, expect a message prop which holds a message that should 
+        be output here. */}
+        <p>{props.message}</p>
       </div>
       <footer>
         {/* In footer want to have button we can click to close the modal */}
